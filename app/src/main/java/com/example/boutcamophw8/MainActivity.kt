129f2f2bb4d96d8c40fc377d91e7e08b20106db4
zahhard <za.darardoust@gmail.com>
2022-03-01 17:18:08 +0330

    a fun to check are edit texts empty and a fun for checking code to be just 10 digits added

--- app/src/main/java/com/example/boutcamophw8/MainActivity.kt
+++ app/src/main/java/com/example/boutcamophw8/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.boutcamophw8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.boutcamophw8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.button.setOnClickListener{
+            codeTypeCheck()
+            checkEmptyView()
+        }
+    }
+
+    private fun codeTypeCheck(){
+        if (binding.myEditTextCode.text?.length != 10){
+            binding.myEditTextCode.error = "نامعتبر"
+        }
+    }
+
+    private fun checkEmptyView(){
+        if (binding.myEditTextCode.text?.isBlank() == true){
+            binding.myEditTextCode.error = "اجباری"
+        }
+        if (binding.myEditTextName.text?.isBlank() == true){
+            binding.myEditTextName.error = "اجباری"
+        }
+        if (binding.myEditText1.text?.isBlank() == true){
+            binding.myEditText1.error = "اجباری"
+        }
+        if (binding.myEditTextBirth.text?.isBlank() == true){
+            binding.myEditTextBirth.error = "اجباری"
+        }
+        if (binding.myEditTextPost.text?.isBlank() == true){
+            binding.myEditTextPost.error = "اجباری"
+        }
+        if (binding.gender.checkedRadioButtonId == -1){
+            binding.men.error = "اجباری"
+        }
+
     }
 }
