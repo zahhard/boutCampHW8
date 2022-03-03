cb3c57cd20db2ae6fa3dd87b6578e4d51cbb4c06
zahhard <za.darardoust@gmail.com>
2022-03-04 01:02:02 +0330

    share preference added

--- app/src/main/java/com/example/boutcamophw8/MainActivity.kt
+++ app/src/main/java/com/example/boutcamophw8/MainActivity.kt
@@ -5,64 +5,100 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.boutcamophw8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var sharedPreferences: SharedPreferences
+    var isRemember = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        binding.button.setOnClickListener {
-            codeTypeCheck()
-            checkEmptyView()
-            var intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
-        }
-    }
 
-    private fun codeTypeCheck() {
+        sharedPreferences = getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+
+        isRemember = sharedPreferences.getBoolean("CHECKED", false)
+
+        if (isRemember) {
+
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        }
+        else{
+
+
+        }
+
+
+            binding.button.setOnClickListener {
+                codeTypeCheck()
+                checkEmptyView()
+
+                if (!codeTypeCheck() || !checkEmptyView()) {
+
+                    isRemember = true
+
+                    val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                    editor.putString("NAME", binding.myEditTextName.text.toString())
+                    editor.putString("CODE", binding.myEditTextCode.text.toString())
+                    editor.putString("POSTCODE", binding.myEditTextPost.text.toString())
+                    editor.putString("BIRTHPLACE", binding.myEditTextBirth.text.toString())
+                    editor.putString("ADDRESS", binding.myEditText1.text.toString())
+                    editor.putBoolean("CHECKED", true)
+                    editor.apply()
+
+                    Toast.makeText(this, "shared successfully", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, MainActivity2::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+
+        }
+
+
+    private fun codeTypeCheck(): Boolean {
+        var isEmpty = false
         if (binding.myEditTextCode.text?.length != 10) {
             binding.myEditTextCode.error = "10 رقمی"
+            isEmpty = true
         }
+        return isEmpty
     }
 
-    private fun checkEmptyView() {
+    private fun checkEmptyView(): Boolean {
+        var ischecked = false
         if (binding.myEditTextCode.text?.isBlank() == true) {
             binding.myEditTextCode.error = "اجباری"
+            ischecked = true
         }
         if (binding.myEditTextName.text?.isBlank() == true) {
             binding.myEditTextName.error = "اجباری"
+            ischecked = true
         }
         if (binding.myEditText1.text?.isBlank() == true) {
             binding.myEditText1.error = "اجباری"
+            ischecked = true
         }
         if (binding.myEditTextBirth.text?.isBlank() == true) {
             binding.myEditTextBirth.error = "اجباری"
+            ischecked = true
         }
         if (binding.myEditTextPost.text?.isBlank() == true) {
             binding.myEditTextPost.error = "اجباری"
+            ischecked = true
         }
         if (binding.gender.checkedRadioButtonId == -1) {
             binding.men.error = "اجباری"
+            ischecked = true
         }
-    }
 
-    fun saveInShared(name: String,
-                     code: Int,
-                     birthPlace: String,
-                     address: String,
-                     postCode: Int)
-    {
-        val sharedPreferences: SharedPreferences =
-            getSharedPreferences("kotlinSharedPreference", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putString("name", name)
-        editor.putInt("code", code)
-        editor.putString("birthPlace", birthPlace)
-        editor.putString("address", address)
-        editor.putInt("postCode", postCode)
-        editor.apply()
+        return ischecked
     }
 }
